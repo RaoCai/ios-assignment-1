4760b43392c4aa0345ce5dd96ce8ee7a3dd1a2ca
RaoCai.Chelios <cairy004@mymail.unisa.edu.au>
2024-09-15 01:57:48 +0930

    developed on macincloud, a running-well version

--- ios/ios/ViewController.swift
+++ ios/ios/ViewController.swift
@@ -1,36 +1,49 @@
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    // widget connection
     
-    @IBOutlet weak var heading: UILabel!
-    @IBOutlet weak var pickerview: UIPickerView!
-    @IBOutlet weak var resultLabel: UILabel!
-
-    let converter = SizeConverter()
-    let sizes = ["1/4\"", "1/2\"", "3/4\""]
-
+    @IBOutlet weak var sizePicker: UIPickerView!
+    
+    @IBOutlet weak var resultlabel: UILabel!
+    
+    // test data
+    let boltSizes = ["1/4\"", "3/8\"", "1/2\""]
+    let sizeInMM = [6.35, 9.525, 12.7]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        pickerview.delegate = self
-        pickerview.dataSource = self
+        sizePicker.dataSource = self
+        sizePicker.delegate = self
+        resultlabel.text = "select the size"
     }
     
+    // UIPickerView
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
-
+    
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return sizes.count
+        return boltSizes.count
     }
-
+    
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return sizes[row]
+        return boltSizes[row]
     }
-
+    
+    // when the size be selected
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        let selectedSize = sizes[row]
-        let metricSize = converter.convertSize(imperialSize: selectedSize)
-        resultLabel.text = metricSize
+        updateResult(for: row)
+    }
+    
+    // output result
+    func updateResult(for row: Int) {
+        resultlabel.text = "\(sizeInMM[row]) mm"
+    }
+    
+    // convert button
+    @IBAction func convertButtonTapped(_ sender: UIButton) {
+        let selectedRow = sizePicker.selectedRow(inComponent: 0)
+        updateResult(for: selectedRow)
     }
 }
